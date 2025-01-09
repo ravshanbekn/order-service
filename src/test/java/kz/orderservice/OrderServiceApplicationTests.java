5ab78d44e372f5cb933d1ea886fe7a6e30895900
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2025-01-10 02:59:58 +0500

    Initial Commit

---
+++ src/test/java/kz/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package kz.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
