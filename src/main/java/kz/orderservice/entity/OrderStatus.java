dae9d8169514773b71c06982692d20b218e56090
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2025-01-10 04:21:37 +0500

    feature/create-entities-for-tables - created entities Order and Product

---
+++ src/main/java/kz/orderservice/entity/OrderStatus.java
@@ -0,0 +1,7 @@
+package kz.orderservice.entity;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED;
+}
---
+++ src/main/java/kz/orderservice/entity/Product.java
@@ -0,0 +1,33 @@
+package kz.orderservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "products")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+}
